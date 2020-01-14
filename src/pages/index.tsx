16dd9a721576a42a0a154f15a978d3dc5310c952
jtinianow <jermtee@gmail.com>
2020-01-14 14:46:09 -0700

    add about section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,6 +10,11 @@ const cinebodyHero = require('../images/Cinebody-Hero@2x.png')
 const geosureHero = require('../images/GeoSure-Hero@2x.png')
 const spruceLabsHero = require('../images/Spruce-Labs-Hero@2x.png')
 
+const dribbbleIcon = require('../assets/Dribbble-Icon.svg')
+const instagramIcon = require('../assets/Instagram-Icon.svg')
+const behanceIcon = require('../assets/Behance-Icon.svg')
+const twitterIcon = require('../assets/Twitter-Icon.svg')
+
 const caseStudies = {
   heartwood: {
     image: {
@@ -57,6 +62,25 @@ const caseStudies = {
   }
 }
 
+const socials = {
+  dribbble: {
+    link: 'https://dribbble.com/jtini',
+    icon: dribbbleIcon
+  },
+  instagram: {
+    link: 'https://www.instagram.com/jermtee/',
+    icon: instagramIcon
+  },
+  behance: {
+    link: 'https://www.behance.net/jtinianow',
+    icon: behanceIcon
+  },
+  twitter: {
+    link: 'https://twitter.com/jtinianow',
+    icon: twitterIcon
+  }
+}
+
 const IndexPage = () => (
   <Layout className="landing-page">
     <SEO title="Tin Yeah No" />
@@ -83,7 +107,7 @@ const IndexPage = () => (
                 className="case-study-summary__image"
               />
               <div className="case-study-summary__text">
-                <h3 className="case-study-summary__title body bold">{`${cs.title} `}</h3>
+                <h2 className="case-study-summary__title body bold">{`${cs.title} `}</h2>
                 <p className="case-study-summary__description body">{cs.description}</p>
                 <p className="case-study-summary__tags small-body mono c-text-on-light-subdued">{cs.tags}</p>
               </div>
@@ -92,6 +116,69 @@ const IndexPage = () => (
         )
       })}
 
+    </section>
+
+    <section className="landing-page-about">
+      <div className="landing-page-about__inner">
+        <h2 className="landing-page-about__title subtitle display">Who is Jeremy?</h2>
+        <p className="landing-page-about__subtitle subheadline"> I design digital products, visual identities, and the systems that power them.</p>
+        <p className="landing-page-about__body body">I’m also a Web Chair with AIGA Colorado and I co-host meetups with Figma Denver.</p>
+        <div className="landing-page-about__services">
+          <div className="landing-page-about__services-list-wrapper">
+            <h3 className="large-body bold">Things I Make</h3>
+            <ul className="landing-page-about__services-list">
+              <li className="landing-page-about__services-list-item">Design Systems</li>
+              <li className="landing-page-about__services-list-item">Websites</li>
+              <li className="landing-page-about__services-list-item">Digital Products</li>
+              <li className="landing-page-about__services-list-item">Prototypes</li>
+              <li className="landing-page-about__services-list-item">Visual Identities</li>
+            </ul>
+          </div>
+
+          <div className="landing-page-about__services-list-wrapper">
+            <h3 className="large-body bold">Services I Offer</h3>
+            <ul className="landing-page-about__services-list">
+              <li className="landing-page-about__services-list-item">Design Strategy</li>
+              <li className="landing-page-about__services-list-item">Visual Design</li>
+              <li className="landing-page-about__services-list-item">Digital Product Design</li>
+              <li className="landing-page-about__services-list-item">Content Strategy</li>
+              <li className="landing-page-about__services-list-item">Research and Discovery</li>
+            </ul>
+          </div>
+
+          <div className="landing-page-about__services-list-wrapper">
+            <h3 className="large-body bold">Tools I Use</h3>
+            <ul className="landing-page-about__services-list">
+              <li className="landing-page-about__services-list-item">Pen and Paper</li>
+              <li className="landing-page-about__services-list-item">Figma</li>
+              <li className="landing-page-about__services-list-item">Photoshop</li>
+              <li className="landing-page-about__services-list-item">Illustrator</li>
+              <li className="landing-page-about__services-list-item">HTML and CSS</li>
+            </ul>
+          </div>
+
+        </div>
+        <div className="landing-page-about__contact">
+          <p className="landing-page-about__contact-title small-body mono bold">Get in touch</p>
+          <p className="body">If you’re interested in working together, feel free to reach out at <a href="mailto:jeremy@tinyeahno.com">jeremy@tinyeahno.com</a>.</p>
+        </div>
+        <div className="landing-page-about__social">
+          <p className="landing-page-about__social-title small-body mono bold">Get social</p>
+          {Object.keys(socials).map(key => {
+            return (
+              <a
+                className="social-icon-link"
+                href={socials[key].link}
+                target="_blank"
+                key={key}
+              >
+                <img src={socials[key].icon} className="social-icon" />
+              </a>
+            )
+          })}
+        </div>
+        <p className="landing-page-about__copyright small-body">© Jeremy Tinianow 2020</p>
+      </div>
     </section>
   </Layout>
 )
