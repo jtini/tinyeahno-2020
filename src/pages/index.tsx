98c6e62a935a7fd1a677c3aadc4091c91499aef1
jtinianow <jermtee@gmail.com>
2021-05-28 14:50:27 -0600

    cleanup landing page footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -114,66 +114,7 @@ const IndexPage = () => (
 
     <section className="landing-page-about">
       <div className="landing-page-about__inner">
-        <h2 className="landing-page-about__title subtitle display">Who is Jeremy?</h2>
-        <p className="landing-page-about__subtitle subheadline"> I design digital products, visual identities, and the systems that power them.</p>
-        <p className="landing-page-about__body body">I’ve had the pleasure of designing and building digital things for some great organizations including <span className="highlight">American Airlines</span>, <span className="highlight">Gatorade</span>, <span className="highlight">AAA</span>, <span className="highlight">Charles Schwab</span>, <span className="highlight">Braintree</span>, and <span className="highlight">Fruit of the Loom</span>.</p>
-        <p className="landing-page-about__body body">I’m also a <span className="highlight">Web Chair with AIGA Colorado</span> and I co-host meetups with <span className="highlight">Figma Denver</span>.</p>
-        <div className="landing-page-about__services">
-          <div className="landing-page-about__services-list-wrapper">
-            <h3 className="large-body bold">Things I Make</h3>
-            <ul className="landing-page-about__services-list">
-              <li className="landing-page-about__services-list-item">Design Systems</li>
-              <li className="landing-page-about__services-list-item">Websites</li>
-              <li className="landing-page-about__services-list-item">Digital Products</li>
-              <li className="landing-page-about__services-list-item">Prototypes</li>
-              <li className="landing-page-about__services-list-item">Visual Identities</li>
-            </ul>
-          </div>
-
-          <div className="landing-page-about__services-list-wrapper">
-            <h3 className="large-body bold">Services I Offer</h3>
-            <ul className="landing-page-about__services-list">
-              <li className="landing-page-about__services-list-item">Design Strategy</li>
-              <li className="landing-page-about__services-list-item">Visual Design</li>
-              <li className="landing-page-about__services-list-item">Digital Product Design</li>
-              <li className="landing-page-about__services-list-item">Content Strategy</li>
-              <li className="landing-page-about__services-list-item">Research and Discovery</li>
-            </ul>
-          </div>
-
-          <div className="landing-page-about__services-list-wrapper">
-            <h3 className="large-body bold">Tools I Use</h3>
-            <ul className="landing-page-about__services-list">
-              <li className="landing-page-about__services-list-item">Pen and Paper</li>
-              <li className="landing-page-about__services-list-item">Figma</li>
-              <li className="landing-page-about__services-list-item">Photoshop</li>
-              <li className="landing-page-about__services-list-item">Illustrator</li>
-              <li className="landing-page-about__services-list-item">HTML and CSS</li>
-            </ul>
-          </div>
-
-        </div>
-        <div className="landing-page-about__contact">
-          <p className="landing-page-about__contact-title small-body mono bold">Get in touch</p>
-          <p className="landing-page-about__contact-body body">If you’re interested in working together, feel free to reach out at <a href="mailto:jeremy@tinyeahno.com" className="highlight">jeremy@tinyeahno.com</a>.</p>
-        </div>
-        <div className="landing-page-about__social">
-          <p className="landing-page-about__social-title small-body mono bold">Get social</p>
-          {Object.keys(socials).map(key => {
-            return (
-              <a
-                className="social-icon-link"
-                href={socials[key].link}
-                target="_blank"
-                key={key}
-                rel="noopener noreferrer"
-              >
-                <img src={socials[key].icon} className="social-icon" alt={`${key} icon`} />
-              </a>
-            )
-          })}
-        </div>
-        <p className="landing-page-about__copyright small-body">© Jeremy Tinianow 2020 · <Link to="/colophon" id="colophon-link">Colophon</Link></p>
+        <p className="landing-page-about__copyright small-body">© Jeremy Tinianow 2021 · <Link to="/colophon" id="colophon-link">Colophon</Link></p>
       </div>
     </section>
   </Layout>
