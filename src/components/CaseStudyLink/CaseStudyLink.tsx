c42c408b51b3c77b16dab4cae5031f3030f530e4
jtinianow <jermtee@gmail.com>
2024-05-01 16:38:25 -0600

    update next case study link

--- src/components/CaseStudyLink/CaseStudyLink.tsx
+++ src/components/CaseStudyLink/CaseStudyLink.tsx
@@ -5,20 +5,20 @@ import Img from 'gatsby-image'
 import './CaseStudyLink.scss'
 
 export interface CaseStudyLinkProps {
-    slug: string,
-    title: string,
-    description: string,
-    tags: string
+  slug: string,
+  title: string,
+  description: string,
+  tags: string
 }
 
 const CaseStudyLink = (props: CaseStudyLinkProps) => {
-    const { slug, title, description, tags } = props
-    return (
-        <div className="case-study-summary">
-            <Link to={`/${slug}`} className="case-study-summary__link">
+  const { slug, title, description, tags } = props
+  return (
+    <div className="case-study-summary">
+      <Link to={`/${slug}`} className="case-study-summary__link">
 
-                <StaticQuery
-                    query={graphql`
+        <StaticQuery
+          query={graphql`
                             query ThumbnailImagesQuery {
                                 cinebody: file(relativePath: { eq: "Cinebody-Thumb@2x.png" }) {
                                     childImageSharp {
@@ -48,32 +48,39 @@ const CaseStudyLink = (props: CaseStudyLinkProps) => {
                                         }
                                     }
                                 }
+                                digcolors: file(relativePath: { eq: "DIG-Colors-Thumb@2x.png" }) {
+                                    childImageSharp {
+                                        fluid(maxWidth: 768, quality: 80) {
+                                        ...GatsbyImageSharpFluid_noBase64
+                                        }
+                                    }
+                                }
                             }
                         `}
-                    render={data => {
-                        const key = slug.split('-').join('')
-                        if (
-                            data[key] &&
-                            typeof data[key] !== 'undefined' &&
-                            data[key].childImageSharp &&
-                            data[key].childImageSharp.fluid) {
+          render={data => {
+            const key = slug.split('-').join('')
+            if (
+              data[key] &&
+              typeof data[key] !== 'undefined' &&
+              data[key].childImageSharp &&
+              data[key].childImageSharp.fluid) {
 
-                            return (
-                                <Img fluid={data[key].childImageSharp.fluid} className="case-study-summary__image" />
-                            )
-                        }
-                        return null
-                    }
-                    }
-                />
-                <div className="case-study-summary__text">
-                    <h2 className="case-study-summary__title body bold">{`${title} `}</h2>
-                    <p className="case-study-summary__description body">{description}</p>
-                    <p className="case-study-summary__tags small-body mono c-text-on-light-subdued">{tags}</p>
-                </div>
-            </Link>
+              return (
+                <Img fluid={data[key].childImageSharp.fluid} className="case-study-summary__image" />
+              )
+            }
+            return null
+          }
+          }
+        />
+        <div className="case-study-summary__text">
+          <h2 className="case-study-summary__title body bold">{`${title} `}</h2>
+          <p className="case-study-summary__description body">{description}</p>
+          <p className="case-study-summary__tags small-body mono c-text-on-light-subdued">{tags}</p>
         </div>
-    )
+      </Link>
+    </div>
+  )
 }
 
 export default CaseStudyLink;
